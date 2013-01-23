a486921759d9790bbaa7a3416ac6cd4ac6bbc3f6
Tomasz Jaskowski <tomasz.jaskowski@polcode.com>
2013-01-23 09:49:01 +0100

    added distributable version

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import os
+
+CURRENT_DIR = os.path.dirname(__file__)
+
+setup(
+    author='Tomasz Jaskowski',
+    author_email='tadeck@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries',
+    ],
+    description='Module for generating and validating HOTP and TOTP tokens',
+    license='MIT',
+    long_description=open(os.path.join(CURRENT_DIR, 'README.rst')).read(),
+    name='onetimepass',
+    packages=['onetimepass'],
+    url='https://github.com/tadeck/onetimepass',
+    version='0.1.2',
+)
