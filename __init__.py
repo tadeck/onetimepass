75ffe2437232dd7bd61eed8a8b94bb2ee664ebee
Tomasz Jaskowski <tomasz.jaskowski@polcode.com>
2013-01-23 09:11:41 +0100

    added new file for backward compatibility

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+File maintained for backward compatibility with previous, non-PyPI versions.
+Thanks to it, you should be able to download and use this module without
+installing it as one of the packages.
+
+It is, however, highly encouraged to use virtualenv or similar solutions.
+"""
+from .onetimepass import *
