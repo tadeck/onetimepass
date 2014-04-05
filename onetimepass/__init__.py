0ec690e0b114426de70c6babfd15823107bba8d5
Tom Jaskowski <tadeck@gmail.com>
2014-04-05 12:32:40 +0200

    sample noop commit no. 1

--- onetimepass/__init__.py
+++ onetimepass/__init__.py
@@ -3,6 +3,8 @@ onetimepass module is designed to work for one-time passwords - HMAC-based and
 time-based. It is compatible with Google Authenticator application and
 applications based on it.
 
+This is one line.
+
 @version: 0.2.2
 @author: Tomasz Jaskowski
 @contact: http://github.com/tadeck
