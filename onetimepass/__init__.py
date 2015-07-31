tadeck/onetimepass

commit 0f48cf1ee324bf5fd21e2a97acd5b144ebd77f3c

added skipping spaces, if they are given in the secret

onetimepass/__init__.py
@@ -107,6 +107,8 @@ def get_hotp(
     if isinstance(secret, six.string_types):
         # It is unicode, convert it to bytes
         secret = secret.encode('utf-8')
+    # Get rid of all the spacing:
+    secret = secret.replace(' ', '')
     try:
         key = base64.b32decode(secret, casefold=casefold)
     except (TypeError):

tests/__init__.py
@@ -90,6 +90,36 @@ class HotpGenerationTestCase(TestCase):
             b'816065',
         )
 
+    def test_ignoring_spaces_in_secret(self):
+        """
+        Check if spaces are skipped during generation of the token
+        """
+        def chunks(original, size):
+            """
+            Split original string into chunks sized as requested in ``size``
+            parameter. Returns iterator yielding single chunks.
+            Similar to: http://stackoverflow.com/a/312464/548696
+            :param original: original string (almost any iterable really)
+            :type original: str or unicode
+            :param size: requested size of chunks
+            :type size: int
+            """
+            for i in six.moves.range(0, len(original), size):
+                yield original[i:i+size]
+
+        # Simple generation without spaces:
+        secret = 'MFRGGZDFMZTWQ2LK'
+        # Simple generation with spaces:
+        secret_with_spaces = ' '.join(chunks(secret, 3))
+        # Check if was properly sliced:
+        self.assertEqual(5, secret_with_spaces.count(' '))
+
+        # Both spaceless secret and secret with spaces should give the same
+        self.assertEqual(
+            get_hotp(secret, 1),
+            get_hotp(secret_with_spaces, 1),
+        )
+
 
 class HotpValidityTestCase(TestCase):
     """

commit 65bc9b2c1fa89f35f0dc664742996d4742ef35f5

fix for Python 3 issue introduced by commit 0f48cf1ee324bf5fd21e2a97acd5b144ebd77f3c

onetimepass/__init__.py
@@ -108,7 +108,7 @@ def get_hotp(
         # It is unicode, convert it to bytes
         secret = secret.encode('utf-8')
     # Get rid of all the spacing:
-    secret = secret.replace(' ', '')
+    secret = secret.replace(b' ', b'')
     try:
         key = base64.b32decode(secret, casefold=casefold)
     except (TypeError):